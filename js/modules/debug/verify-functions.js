f33601ee26d53ff7a87629b635f1d1030bfd8d2d
rclessa25-hub <rclessa25@gmail.com>
2026-01-08 20:09:00 -0300

    Delete js/modules/debug/verify-functions.js

--- js/modules/debug/verify-functions.js
+++
@@ -1,6 +0,0 @@
-// ARQUIVO REMOVIDO - Migrado para repositório de suporte
-// Ver: https://github.com/rclessa25-hub/weberlessa-support/tree/main/debug
-console.log('📁 verify-functions.js removido - migrado para repositório de suporte');
-
-// Este arquivo mantido apenas para compatibilidade
-// O código real está em: https://rclessa25-hub.github.io/weberlessa-support/debug/function-verifier.js
